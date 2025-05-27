ff6633b0fd86eeca3a7261548ebbf4444d44e2ef
gabe <gzolot@calpoly.edu>
2025-05-27 11:16:31 -0700

    Added CR1 and CR2 Register values for SPI2

--- Core/Src/spi.c
+++ Core/Src/spi.c
@@ -14,8 +14,8 @@ void spiInit(){
 	SCK_GPIO->MODER  &= ~(0b11 << (SCK_OFFSET * 2));
 	SCK_GPIO->MODER  |=  (0b10 << (SCK_OFFSET * 2));   	// Alternate function mode
 
-	NSS_GPIO->MODER  &= ~(0b11 << (NSS_OFFSET * 2));
-	NSS_GPIO->MODER  |=  (0b10 << (NSS_OFFSET * 2));   	// Alternate function mode
+	NSS_GPIO->MODER &= ~(0b11 << (NSS_OFFSET * 2));
+	NSS_GPIO->MODER |=  (0b01 << (NSS_OFFSET * 2));  	// Output mode because software slave management
 
 	// Set speed for GPIO
 	MOSI_GPIO->OSPEEDR |= (0b11 <<(MOSI_OFFSET * 2));	// Very high speed
@@ -36,9 +36,18 @@ void spiInit(){
 	MISO_GPIO->AFR[1] &= ~(0xF << ((MISO_OFFSET % 8) * 4));
 	MISO_GPIO->AFR[1] |= (0x5 << ((MISO_OFFSET % 8) * 4));
 
-	NSS_GPIO->AFR[1] &= ~(0xF << ((NSS_OFFSET % 8) * 4));
-	NSS_GPIO->AFR[1] |= (0x5 << ((NSS_OFFSET % 8) * 4));
+	SPI2->CR1 &= ~(SPI_CR1_BR);			//ABP1 frequency % 2, baud rate of 2MHz
+	SPI2->CR1 &= ~(SPI_CR1_CPHA); 		//CPHA = 0
+	SPI2->CR1 &= ~(SPI_CR1_CPOL); 		//CPOL = 0
+	SPI2->CR1 &= ~(SPI_CR1_LSBFIRST); 	//MSB first
+	SPI2->CR1 |= SPI_CR1_MSTR;			//microcontroller set to master
+	SPI2->CR1 |= SPI_CR1_SSM;			//software slave management
+	SPI2->CR1 |= SPI_CR1_SSI; 			//this registers sets or clears NSS, NSS is idle high
+	SPI2->CR1 |= SPI_CR1_SPE;			//SPI enable
 
-	SPI2->CR1 &= ~(SPI_CR1_BR); //ABP1 frequency % 2, baud rate of 2MHz
-
+	SPI2->CR2 = 0;
+	SPI2->CR2 |= (1 << SPI_CR2_FRXTH_Pos);      // RXNE event when FIFO level >= 8-bit
+	SPI2->CR2 |= (7 << SPI_CR2_DS_Pos);
 }
+
+
